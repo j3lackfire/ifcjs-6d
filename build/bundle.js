dce8b6866d8b19efe98fd8c38e1d1ba3d282d464
j3lackfire <j3lackfire@gmail.com>
2022-11-26 23:20:29 +0200

    Still does not work, remove all gypsum

--- build/bundle.js
+++ build/bundle.js
@@ -106017,8 +106017,8 @@ const emission = {
   "Rigid insulation": 64,
   // "Air": 1,
   "Aluminum": 157.5,
-  "Metal - Painted - Grey": 123.9,
-  "Metal Door - Brindle": 123.9,
+//   "Metal - Painted - Grey": 123.9,
+//   "Metal Door - Brindle": 123.9,
   "Cherry": 1,
   "Glass": 1200,
   "Wood - Birch - Solid Stained Light Low Gloss": 44.8, //448.8
