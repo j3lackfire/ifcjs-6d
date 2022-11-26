879c374c45ebf8ce301dfc5cbc72383db68f42de
j3lackfire <j3lackfire@gmail.com>
2022-11-26 23:37:16 +0200

    Delete all, cmon

--- build/bundle.js
+++ build/bundle.js
@@ -106010,21 +106010,21 @@ const emission = {
   // "Brick, Common": 300,
 //   "Concrete, Lightweight": 201.6,
   // "Gypsum Wall Board": 31.155,
-  "Sand": 7.5,
+//   "Sand": 7.5,
 //   "Concrete, Precast": 67,
-  "Damp-proofing": 1,
-  "Plaster": 1,
+//   "Damp-proofing": 1,
+//   "Plaster": 1,
   // "Rigid insulation": 64,
   // "Air": 1,
   // "Aluminum": 157.5,
   // "Metal - Painted - Grey": 123.9,
   // "Metal Door - Brindle": 123.9,
-  "Cherry": 1,
+//   "Cherry": 1,
   // "Glass": 1200,
   // "Wood - Birch - Solid Stained Light Low Gloss": 44.8, //448.8
-  "Paint - Sienna": 1,
-  "Fenster - Kunststoff wei": 52,
-  "Fenster - Kunststoff": 52,
+//   "Paint - Sienna": 1,
+//   "Fenster - Kunststoff wei": 52,
+//   "Fenster - Kunststoff": 52,
 };
 
 function getEmission(material) {
