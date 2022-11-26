f67671255544236389d72cd77cb70e737bd1058d
j3lackfire <j3lackfire@gmail.com>
2022-11-26 23:14:09 +0200

    New data works, just a bit, well, not visually attractive

--- functions/emission.js
+++ functions/emission.js
@@ -13,10 +13,10 @@ const emission = {
   "Metal Door - Brindle": 123.9,
   "Cherry": 1,
   "Glass": 1200,
-  "Wood - Birch - Solid Stained Light Low Gloss": 448.8,
+  "Wood - Birch - Solid Stained Light Low Gloss": 44.8, //448.8
   "Paint - Sienna": 1,
   "Fenster - Kunststoff wei": 52,
-  "Fenster - Kunststoff": -52,
+  "Fenster - Kunststoff": 52,
 };
 
 export default function getEmission(material) {
